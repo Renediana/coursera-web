7db8b380ff6de7f8c8600c2d3f0224c5de1b7901
René Diana <dianarene01@gmail.com>
2020-10-20 18:20:43 +0200

    Added first javascript code to test site

---
+++ script.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded",
+    function ( event ){
+        function sayHello () {
+            this.textContent = "Said it!";
+            var name = 
+                document.getElementById("name").value
+                var message = "<h2>Hello " +  name + "!</h2>";
+            
+                document
+                    .getElementById("content")
+                    .innerHTML = message;
+        
+            if (name === "student"){
+                var title = 
+                    document
+                        .querySelector("#title")
+                        .textContent;
+                title += " & Lovin' it!";
+                document
+                    .querySelector("#title")
+                    .textContent = title;
+            }
+        }
+        
+        document.querySelector("button")
+                .addEventListener("click", sayHello);
+    }
+);
